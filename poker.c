a906c05b15cc3497350bde36f6760f37d84b59b0
Reimuduck <144057631+ReimuDuck@users.noreply.github.com>
2025-10-19 21:47:49 -0700

    Poker Update

--- poker.c
+++ poker.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <conio.h>
 
+#define DECKSIZE 52
 typedef enum { CLUBS, DIAMONDS, HEARTS, SPADES, CARD } Suit;
 typedef enum { TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE, CARD2 } Rank;
 
@@ -12,73 +13,63 @@ typedef struct {
     Suit suit;
     Rank rank;
 } Card;
-Card deck[52];
-Card hand[52];
+Card deck[DECKSIZE];
+Card hand[DECKSIZE];
 Card empty = {CARD, CARD2};
 int deckSize;
 
-// Function to print a card (for demonstration)
+
 void printCard(Card c) {
     const char* suitNames[] = {"Clubs", "Diamonds", "Hearts", "Spades", "BLANK"};
     const char* rankNames[] = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace", "BLANK"};
     printf("%s of %s\n", rankNames[c.rank], suitNames[c.suit]);
     return;
 }
-void printDeck(Card deck[52]){
+void printDeck(void){
     int deckpos = 0;
-    for(int i = 0; i <= 3; i++){
-        for (int j = 0; j <= 12; j++){
-            
+    for(int i = CLUBS; i <= SPADES; i++){
+        for (int j = TWO; j <= ACE; j++){
+            deck[deckpos].suit = i;
+            deck[deckpos].rank = j;
             deckpos++;
-            Card c = {i, j};
-            deck[deckpos] = c;
-
         }
     }
-    for(int i = 0; i < 52; i++){
-        hand[i].suit = CARD;   
-        hand[i].rank = CARD2;
-        deck[i];
-        if(deck[i].suit == 4 || deck[i].rank == 13){
-            continue;
-        }else{
-            deckSize++;
-        }
+    for (int i = 0; i < DECKSIZE; i++) {
+        hand[i] = empty;
     }
+    deckSize = DECKSIZE;
     printf("Deck initialized... \n");
     printf("\n");
     return;
 
 }
-void countDeck(Card deck[52]){
+void countDeck(void){
     deckSize = 0;
 
-    for(int i = 0; i < 52; i++){
-    
+    for(int i = 0; i < DECKSIZE; i++){
+        
         deck[i];
-        if(deck[i].suit == 4 || deck[i].rank == 13){
-            continue;
-        }else{
+        if(!(deck[i].suit == 4 && deck[i].rank == 13)){
             deckSize++;
         }
     }
     printf("Deck size: %d \n",deckSize);
     return;
 }
-void drawCard(Card deck[52]){
+void drawCard(void){
     printf(" \nCard Drawn: \n");
-    for(int i = 0; i < 52; i++){
+    for(int i = 0; i < DECKSIZE; i++){
         
         if(hand[i].suit == CARD || hand[i].rank == CARD2){
-            int index = rand() % (52 - 0 + 1) + 0;
+            int index =  rand() % DECKSIZE;
             while (deck[index].suit == CARD && deck[index].rank == CARD2) {
-                index = rand() % 52;
+                index = rand() % DECKSIZE;
             }
             hand[i]= deck[index];
             deck[index].suit = CARD;  
             deck[index].rank = CARD2;
             printCard(hand[i]);
-            countDeck(deck); 
+            countDeck(); 
             break; 
         }else{
             continue;
@@ -89,37 +80,39 @@ void drawCard(Card deck[52]){
 
     return;
 }
-void printHand(Card hand[52]){
+void printHand(void){
     printf("\nHand: \n");
-    int hands = 52-deckSize;
+    int hands = DECKSIZE-deckSize;
     for(int i = 0; i < hands; i++){
         printCard(hand[i]);
     }
     return;
 }
 
-void startGame(Card deck[52], Card hand[52]){
+void startGame(void){
 
     srand(time(NULL)); 
-    bool deal = true;
-    countDeck(deck);
-    printf("HAND: \n");
+    countDeck();
+    printf("Dealing 5 cards... \n");
     for(int i = 0; i < 5; i++){
-        int index = rand() % (52 - 0 + 1) + 0;
+        int index = rand() % DECKSIZE;
+        
+        while (deck[index].suit == CARD && deck[index].rank == CARD2)
+            index = rand() % DECKSIZE;
         
         hand[i]= deck[index];
         deck[index] = empty;
         printCard(hand[i]);
     }
-    countDeck(deck);
+    countDeck();
     printf("Turn 1: \n");
-    drawCard(deck);
+    drawCard();
     return;
 }
 int main() {
     char ch;
-    printDeck(deck);
-    startGame(deck, hand);
+    printDeck();
+    startGame();
 
 
     printf("Press 'd' to draw, 'h' to show hand, 'q' to quit\n");
@@ -127,11 +120,12 @@ int main() {
         if (_kbhit()) {       // checks if a key was pressed
             ch = _getch();    // gets the key without waiting for Enter
 
-            if (ch == 'd') drawCard(deck);
-            else if (ch == 'h') printHand(hand);
+            if (ch == 'd') drawCard();
+            else if (ch == 'h') printHand();
             else if (ch == 'q') break;  // quit the loop
         }
     }
+    printf("\nExiting...\n");
 
     return 0;
 }
